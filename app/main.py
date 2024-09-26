3f7e2f0d3c91f993aedb1fe5594c025f2b38f16c
its-anushz <anushreekc11@gmail.com>
2024-09-26 20:05:07 +0530

    ^ characters

--- app/main.py
+++ app/main.py
@@ -12,6 +12,8 @@ def match_pattern(input_line, pattern):
     elif pattern == "\\w":
         return any(c.isalnum() for c in input_line)
     elif pattern[0] == "[" and pattern[-1] == "]":
+        if pattern[1] == "^":
+            return not any(char in pattern[1:-1] for char in input_line)
         return any(char in pattern[1:-1] for char in input_line)
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
