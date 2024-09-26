e8ad7fbf1baddf9d9e67d25045de2998818dbc9b
its-anushz <anushreekc11@gmail.com>
2024-09-26 20:38:18 +0530

    change 2

--- app/main.py
+++ app/main.py
@@ -41,62 +41,74 @@ if __name__ == "__main__":
     main()'''
 
 import sys
-
-# Recursive matcher function to handle patterns like \d and \w
+# import pyparsing - available if you need it!
+# import lark - available if you need it!
 def matcher(input_line, pattern):
     ptr1 = 0
     ptr2 = 0
-
     if input_line == "" and pattern == "":
         return True
-    elif input_line == "" and pattern != "":
+    elif input_line == "":
         return False
-    elif input_line != "" and pattern == "":
+    elif pattern == "":
         return True
-
-    while ptr1 < len(input_line) and ptr2 < len(pattern):
-        if ptr2 + 1 < len(pattern) and pattern[ptr2:ptr2 + 2] == "\\d":
+    while ptr1 < len(input_line):
+        if ptr2 + 1 < len(pattern) and pattern[ptr2 : ptr2 + 2] == "\\d":
             if input_line[ptr1].isdigit():
-                return matcher(input_line[ptr1 + 1:], pattern[ptr2 + 2:])
+                return matcher(input_line[ptr1 + 1 :], pattern[ptr2 + 2 :])
             else:
-                ptr1 += 1
-        elif ptr2 + 1 < len(pattern) and pattern[ptr2:ptr2 + 2] == "\\w":
+                ptr1 = ptr1 + 1
+        elif ptr2 + 1 < len(pattern) and pattern[ptr2 : ptr2 + 2] == "\\w":
             if input_line[ptr1].isalnum():
-                return matcher(input_line[ptr1 + 1:], pattern[ptr2 + 2:])
+                return matcher(input_line[ptr1 + 1 :], pattern[ptr2 + 2 :])
             else:
-                ptr1 += 1
+                ptr1 = ptr1 + 1
         elif input_line[ptr1] == pattern[ptr2]:
-            return matcher(input_line[ptr1 + 1:], pattern[ptr2 + 1:])
+            return matcher(input_line[ptr1 + 1 :], pattern[ptr2 + 1 :])
         else:
-            ptr1 += 1
-
+            ptr1 = ptr1 + 1
     return False
-
-
 def match_pattern(input_line, pattern):
-    # Use recursive matcher for complex patterns like \d, \w
-    return matcher(input_line, pattern)
-
-
+    if len(pattern) == 1:
+        return pattern in input_line
+    elif pattern == "\\d":
+        for i in range(10):
+            if input_line.find(str(i)) != -1:
+                return True
+    elif pattern == "\\w":
+        return input_line.isalnum()
+    elif pattern[0:2] == "[^":
+        pat = pattern[2:-1]
+        for ch in pat:
+            if input_line.find(ch) != -1:
+                return False
+        return True
+    elif pattern[0] == "[" and pattern[-1] == "]":
+        pat = pattern[1:-1]
+        for ch in pat:
+            if input_line.find(ch) != -1:
+                return True
+        return False
+    else:
+        return matcher(input_line, pattern)
 def main():
-    if len(sys.argv) < 3 or sys.argv[1] != "-E":
+    pattern = sys.argv[2]
+    input_line = sys.stdin.read()
+    if sys.argv[1] != "-E":
         print("Expected first argument to be '-E'")
         exit(1)
-
-    pattern = sys.argv[2]
-    input_line = sys.stdin.read().strip()  # strip to remove extra newlines
-
-    # Debug print
-    print(f"Input: {input_line}, Pattern: {pattern}")
-
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    print("Logs from your program will appear here!")
+    # Uncomment this block to pass the first stage
     if match_pattern(input_line, pattern):
         exit(0)
     else:
         exit(1)
-
-
 if __name__ == "__main__":
     main()
+
+
+
 
 
     
